cc1326f4c3be0097ce36ce125ecdc045b6340ce6
lorena_soares1256 <164919178+lorenasoares16@users.noreply.github.com>
2025-08-01 15:17:42 -0300

    .....

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ let nomeUsuario = prompt("Qual o seu nome");
 }
 
 const lista = document.queriSelector('#lista');
+
+lista.textContentextContent = linguagens[0];
+lista.textContentextContent = linguagens[1];
+lista.textContentextContent = linguagens[2];
+
+let aluno1 = {
+    nome:'Lorena Soares',
+    idade:17,
+    anoLetivo: 'ensino medio',
+    materiasFavoritas= ['Biologia', 'Ed:Fisica', 'Matematica II']
+}
+console.log(aluno1.Lorena Soares);
+console.log(aluno1.17);
+console.log(aluno1.'Biologia', 'Ed:Fisica', 'Matematica II');
+console.log(aluno1.Biologia);
